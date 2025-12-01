9e0acb7051c4278ddc3d05c4ab0ae5c8b1eaebac
Wender Carvalho <wendernogueira99@gmail.com>
2025-12-01 19:52:40 -0300

    fix: novo ajuste na url de cloudhumans

--- routes/activity.js
+++ routes/activity.js
@@ -95,7 +95,7 @@ exports.execute = function (req, res) {
                 'api_access_token': `${process.env.CH_AUTHORIZATION_KEY}`
             }
             const guid_id = uuidv4();
-            const domain = cloudchat2;
+            const domain = 'cloudchat2';
             const url = 'https://'+domain+'.cloudhumans.com/api/v1/accounts/'+ account + '/conversations/create_proactive_whatsapp_conversation';
 
             const post_save = {
